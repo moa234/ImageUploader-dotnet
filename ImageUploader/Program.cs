690acbdedd2d8181696d40222dbdde89cedd3d86
Moaaz Tarek <moaaztarek@hotmail.com>
2024-03-27 12:41:55 +0200

    handle antiforgery

--- ImageUploader/Program.cs
+++ ImageUploader/Program.cs
@@ -1,27 +1,38 @@
 using System.Text.Json;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAntiforgery();
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Content("""
-                                      <html>
-                                      <head>
-                                          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                                      </head>
-                                      <body>
-                                          <div class="container">
-                                              <h1>Upload Image</h1>
-                                              <form action="/upload" method="post" enctype="multipart/form-data">
-                                                  <input class="form-control mt-4" type="text" placeholder="Title" name="title" required>
-                                                  <input class="form-control form-control-sm mt-2" type="file" name="file" required>
-                                                  <br/>
-                                                  <button type="submit" class="btn btn-primary">Upload</button>
-                                              </form>
-                                          </div>
-                                      </body>
-                                      </html>
-                                      """, "text/html"));
+app.UseAntiforgery();
+
+app.MapGet("/", (HttpContext context, IAntiforgery antiforgery) =>
+{
+    var token = antiforgery.GetAndStoreTokens(context);
+    return Results.Content(
+        $"""
+         <html>
+         <head>
+             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+         </head>
+         <body>
+             <div class="container">
+                 <h1>Upload Image</h1>
+                 <form action="/upload" method="post" enctype="multipart/form-data">
+                     <input name="{token.FormFieldName}" type="hidden" value="{token.RequestToken}" />
+                     <input class="form-control mt-4" type="text" placeholder="Title" name="title" required>
+                     <input class="form-control form-control-sm mt-2" type="file" name="file" required>
+                     <br/>
+                     <button type="submit" class="btn btn-primary">Upload</button>
+                 </form>
+             </div>
+         </body>
+         </html>
+         """, "text/html");
+});
+
 
 app.MapPost("/upload", async (IFormFile file, [FromForm] string title) =>
 {
@@ -52,7 +63,7 @@ app.MapPost("/upload", async (IFormFile file, [FromForm] string title) =>
     await File.WriteAllTextAsync(jsonPath, JsonSerializer.Serialize(data));
 
     return await Task.FromResult(Results.Redirect($"/picture/{id}"));
-}).DisableAntiforgery();
+});
 
 app.MapGet("/pictureFile/{id}", (string id) =>
 {
@@ -61,6 +72,7 @@ app.MapGet("/pictureFile/{id}", (string id) =>
     {
         return Results.NotFound();
     }
+
     var stream = File.OpenRead(data["path"]);
     return Results.File(stream, data["contentType"]);
 });
@@ -109,5 +121,6 @@ Dictionary<string, string>? GetImageData(string id)
     {
         return null;
     }
+
     return data?[id];
 }
